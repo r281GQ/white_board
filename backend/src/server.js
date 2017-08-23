2070092ca4a1ad04d24d08a80b60d25357bcac1c
Endre Vegh <endre.vegh@yahoo.com>
2017-08-24 00:59:13 +0100

    fix(): express use func

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get(express.static(path.join(__dirname, '/../../build')));
+app.use(express.static(path.join(__dirname, '/../../build')));
 
 app.listen(PORT, () => console.log());
