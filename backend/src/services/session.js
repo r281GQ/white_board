aed1fff57b56d2b89b03d7a5cb7844021160c644
Endre Vegh <endre.vegh@yahoo.com>
2017-08-24 23:52:21 +0100

    fix(): url parser

--- backend/src/services/session.js
+++ backend/src/services/session.js
@@ -1,8 +1,26 @@
 module.exports = app => {
+  const { URL } = require('url');
   const redis = require('redis');
   const session = require('express-session');
   const redisStore = require('connect-redis')(session);
   const config = require('./../config')();
+
+  let store;
+
+  if (process.env.NODE_ENV !== 'production') {
+    const myURL = new URL(process.env.REDISTOGO_URL);
+    store = {
+      host: myURL.host,
+      port: myURL.port,
+      db: myURL.username,
+      pass: myURL.password
+    };
+  } else {
+    store = {
+      client: redis.createClient(),
+      ttl: 260
+    };
+  }
 
   let sessionConfig = {
     cookie: {
@@ -11,31 +29,28 @@ module.exports = app => {
       secure: true
     },
     secret: config.cookie_secret,
-    store: new redisStore({
-      url: process.env.REDISTOGO_URL,
-      ttl: 260
-    }),
+    store: new redisStore(store),
     saveUninitialized: false,
     resave: false
   };
 
   if (process.env.NODE_ENV !== 'production') {
     sessionConfig.cookie.secure = false;
-    sessionConfig.cookie.store = new redisStore({
-      client: redis.createClient(),
-      ttl: 260
-    });
+    // sessionConfig.cookie.store = new redisStore({
+    //   client: redis.createClient(),
+    //   ttl: 260
+    // });
   }
 
   app.use(session(sessionConfig));
-
-  return process.env.NODE_ENV !== 'production'
-    ? new redisStore({
-        url: process.env.REDISTOGO_URL,
-        ttl: 260
-      })
-    : new redisStore({
-        client: redis.createClient(),
-        ttl: 260
-      });
+  return store;
+  // return process.env.NODE_ENV !== 'production'
+  //   ? new redisStore({
+  //       url: process.env.REDISTOGO_URL,
+  //       ttl: 260
+  //     })
+  //   : new redisStore({
+  //       client: redis.createClient(),
+  //       ttl: 260
+  //     });
 };
