a89874b252771b3b3f484a122d37fc025a752da0
Endre Vegh <endre.vegh@yahoo.com>
2017-08-25 15:37:20 +0100

    add(): express session returned

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,11 +9,85 @@ const morgan = require('morgan')
 // const config = require('./config')();
 
 
-const session = require('cookie-session')
+// const session = require('cookie-session')
 // const redisStore = require('connect-redis')(session);
 const config = require('./config')();
 
+
+
+
+
+const { URL } = require('url');
+const redis = require('redis');
+const session = require('express-session');
+const redisStore = require('connect-redis')(session);
+// const config = require('./../config')();
+
 let store;
+
+// app.use(session({maxAge: 60*60*10000, keys: [config.cookie_secret]}))
+//
+if (process.env.NODE_ENV === 'production') {
+  console.log(process.env.REDISTOGO_URL);
+  const myURL = new URL(process.env.REDISTOGO_URL);
+  store = {
+    host: myURL.hostname,
+    port: myURL.port,
+    db: myURL.username,
+    pass: myURL.password,
+    ttl: 260
+  };
+} else {
+  store = {
+    client: redis.createClient(),
+    ttl: 260
+  };
+}
+
+let sessionConfig = {
+  cookie: {
+    httpOnly: true,
+    maxAge: 60 * 60 * 1000,
+    secure: true,
+    path: '/'
+  },
+  secret: config.cookie_secret,
+  store: new redisStore(store),
+  saveUninitialized: false,
+  resave: false,
+  proxy: true
+};
+
+if (process.env.NODE_ENV !== 'production') {
+  sessionConfig.cookie.secure = false;
+  sessionConfig.cookie.store = new redisStore({
+    client: redis.createClient(),
+    ttl: 260
+  });
+}
+// return null;
+// console.log(sessionConfig.store);
+// return sessionConfig.store;
+// return process.env.NODE_ENV !== 'production'
+//   ? new redisStore({
+//       url: process.env.REDISTOGO_URL,
+//       ttl: 260
+//     })
+//   : new redisStore({
+//       client: redis.createClient(),
+//       ttl: 260
+//     });
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -25,8 +99,9 @@ app.enable('trust proxy');
 app.use(morgan('combined'))
 app.use(bodyParser.json());
 // app.use(cookieParser(config.cookie_secret))
+app.use(session(sessionConfig));
 
-app.use(session({maxAge: 60*60*10000, keys: ['sdfsdfsd'], secure:false}))
+// app.use(session({maxAge: 60*60*10000, keys: ['sdfsdfsd'], secure:false}))
 
 console.log(app);
 
