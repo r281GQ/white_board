b378028595e07a5d3e76cd60033bf921bd374698
Endre Vegh <endre.vegh@yahoo.com>
2017-08-25 12:43:29 +0100

    ref(): moved session to main

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,15 @@ const cookieParser = require('cookie-parser');
 const config = require('./config')();
 
 
+const session = require('cookie-parser')
+// const redisStore = require('connect-redis')(session);
+const config = require('./config')();
+
+let store;
+
+app.use(session({maxAge: 60*60*10000, keys: [config.cookie_secret]}))
+
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -19,7 +28,7 @@ app.use(cookieParser(config.cookie_secret))
 
 require('./services/cors')(app);
 
-const store = require('./services/session')(app);
+// const store = require('./services/session')(app);
 
 app.use(bodyParser.json());
 
